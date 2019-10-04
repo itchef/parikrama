9b1d9345df046de076509f1fe02dce948ac25424
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-10-04 18:07:37 +0530

    Updates

---
+++ precache-manifest.40bc8a04fc3f3446d669ea37a00e1780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cb0bf50ffc875b739019b163f9abfe",
+    "url": "/parikrama/index.html"
+  },
+  {
+    "revision": "6b095e2e48657f31dbda",
+    "url": "/parikrama/static/css/main.27b2c460.chunk.css"
+  },
+  {
+    "revision": "a06845223f3743188bdb",
+    "url": "/parikrama/static/js/2.92dc0c75.chunk.js"
+  },
+  {
+    "revision": "6b095e2e48657f31dbda",
+    "url": "/parikrama/static/js/main.fd53b49d.chunk.js"
+  },
+  {
+    "revision": "0b62f6bdf53df5f5db38",
+    "url": "/parikrama/static/js/runtime-main.c4444847.js"
+  }
+]);
