b2c159685cb77c988fbeff7d9ccd920af22e9ef9
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-10-04 18:02:23 +0530

    Kaustav | #008 | Created parikrama website.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App__wrapper">
+      <img src={logo} alt="Parikrama Logo" className="App__logo" />
+      <p className="App__description">
+        Parikrama, is a mobile app to get possible list for Durga Puja 2019
+      </p>
+      <a
+        className="App__button"
+        href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40phoenixtw/parikrama-mobile-9388b2c564774f8785a23f8d41b084f4-signed.apk"
+      >
+        Download
+      </a>
     </div>
   );
 }
